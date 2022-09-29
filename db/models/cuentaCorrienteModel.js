32fa1e6fd6509ac77da2ce74c0d9e93482cef192
Gary Núñez <gnunezc@unsa.edu.pe>
2022-09-28 22:40:13 -0500

    Exchange list implemented

---
+++ db/models/cuentaCorrienteModel.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const CUENTA_TABLE = 'cuentas_corrientes';
+
+const CuentaSchema = {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.BIGINT
+    },
+    numeroCuenta: {
+        field: 'numero_cuenta',
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    banco: {
+        field: 'banco',
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    moneda: {
+        field: 'moneda',
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    descripcion: {
+        field: 'descripcion',
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+}
+
+class CuentaCorriente extends Model {
+    static associate(models) {
+        
+
+    }
+
+    static config(sequelize) {
+        return {
+            sequelize,
+            tableName: CUENTA_TABLE,
+            modelName: 'CuentaCorriente',
+        }
+    }
+}
+
+module.exports = { CUENTA_TABLE, CuentaSchema, CuentaCorriente }
--- db/models/index.js
+++ db/models/index.js
@@ -6,9 +6,12 @@ const { Administrado, AdministradoSchema } = require('./administradoModel');
 const { TipoPago, TipoPagoSchema } = require('./tipoPagoModel');
 const { Actescu, ActescuSchema } = require('./actescuModel');
 const { Actespe, ActespeSchema } = require('./actespeModel');
+const { Canje, CanjeSchema } = require('./canjeModel');
+const { Comprobante, ComprobanteSchema } = require('./comprobanteModel');
+const { CuentaCorriente, CuentaCorrienteSchema } = require('./cuentaCorrienteModel');
 
 
-function setupModels(mainDB, siac, banco, unsapay) {
+function setupModels(mainDB, siac, banco, unsapay, siscaja) {
     //Se inicializan modelos
     User.init(UserSchema, User.config(mainDB));
     Actmail.init(ActmailSchema, Actmail.config(siac));
@@ -18,12 +21,16 @@ function setupModels(mainDB, siac, banco, unsapay) {
     TipoPago.init(TipoPagoSchema, TipoPago.config(unsapay));
     Actescu.init(ActescuSchema, Actescu.config(siac));
     Actespe.init(ActespeSchema, Actespe.config(siac));
+    Canje.init(CanjeSchema, Canje.config(siscaja));
+    Comprobante.init(ComprobanteSchema, Comprobante.config(siscaja));
+    CuentaCorriente.init(CuentaCorrienteSchema, CuentaCorriente.config(siscaja));
 
     //Se inicializan relaciones de los modelos
     User.associate(siac.models);
     TipoPago.associate(unsapay.models);
     Concepto.associate(unsapay.models);
     Banco.associate(unsapay.models, siac.models);
+    Canje.associate(siscaja.models);
 }
 
 module.exports = setupModels;
